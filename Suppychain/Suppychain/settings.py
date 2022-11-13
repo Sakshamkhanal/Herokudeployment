3a6ce4a2e48b40cee52e872fc53a3ac0822236f1
SakshamKhanal <Saksham.khanal01@gmail.com>
2022-11-13 13:57:23 +0545

    Settings modification

--- Suppychain/Suppychain/settings.py
+++ Suppychain/Suppychain/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Suppychain.urls'
+ROOT_URLCONF = 'Supplychain.urls'
 
 TEMPLATES = [
     {
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Suppychain.wsgi.application'
+WSGI_APPLICATION = 'Supplychain.wsgi.application'
 
 
 # Database
